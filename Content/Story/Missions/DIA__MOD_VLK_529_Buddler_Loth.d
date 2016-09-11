6c85766b887f0d98d4d84af2f3d92709beef197d
wyzyskk <luna4@onet.eu>
2016-09-11 16:08:37 +0200

    Update DIA__MOD_VLK_529_Buddler_Loth.d
    Dałem permanenta dla opcji "Co słychać?" i dla opcji "Co możesz mi powiedzieć o tym miejscu?". Powód? Jeżeli się wykorzystało te opcje to postać do końca gry miała jedynie opcje koniec i była niema, a to źle wygląda. Te dwie opcje nadawały się na perma więc je tak ustawiłem. Nie wiem czy dobrze to wykonałem - zamieniłem "permanent = FALSE" na "permanent = TRUE"

--- Content/Story/Missions/DIA__MOD_VLK_529_Buddler_Loth.d
+++ Content/Story/Missions/DIA__MOD_VLK_529_Buddler_Loth.d
@@ -33,7 +33,7 @@ INSTANCE DIA_Buddler_LOTH1 (C_INFO)
    condition    = DIA_Buddler_LOTH1_Condition;
    information  = DIA_Buddler_LOTH1_Info;
    permanent	= FALSE;
-   description	= "Kim jesteœ?";
+   description	= "Kim jesteÅ“?";
 };
 
 FUNC INT DIA_Buddler_LOTH1_Condition()
@@ -43,10 +43,10 @@ FUNC INT DIA_Buddler_LOTH1_Condition()
 
 FUNC VOID DIA_Buddler_LOTH1_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_LOTH1_15_01"); //Kim jesteœ?
-    AI_Output (self, other ,"DIA_Buddler_LOTH1_03_02"); //A co je¿eli powiedzia³bym, ¿e jestem Loth – robotnik porwany z portu w Vengardzie?
-    AI_Output (other, self ,"DIA_Buddler_LOTH1_15_03"); //Odpowiedzia³bym, ¿e ja jestem skazañcem.
-    AI_Output (self, other ,"DIA_Buddler_LOTH1_03_04"); //Wiêc jestem Loth.
+    AI_Output (other, self ,"DIA_Buddler_LOTH1_15_01"); //Kim jesteÅ“?
+    AI_Output (self, other ,"DIA_Buddler_LOTH1_03_02"); //A co jeÂ¿eli powiedziaÂ³bym, Â¿e jestem Loth â€“ robotnik porwany z portu w Vengardzie?
+    AI_Output (other, self ,"DIA_Buddler_LOTH1_15_03"); //OdpowiedziaÂ³bym, Â¿e ja jestem skazaÃ±cem.
+    AI_Output (self, other ,"DIA_Buddler_LOTH1_03_04"); //WiÃªc jestem Loth.
 };
 
 //========================================
@@ -60,7 +60,7 @@ INSTANCE DIA_Buddler_LOTH2 (C_INFO)
    condition    = DIA_Buddler_LOTH2_Condition;
    information  = DIA_Buddler_LOTH2_Info;
    permanent	= FALSE;
-   description	= "Od dawna tu jesteœ? ";
+   description	= "Od dawna tu jesteÅ“? ";
 };
 
 FUNC INT DIA_Buddler_LOTH2_Condition()
@@ -74,11 +74,11 @@ FUNC INT DIA_Buddler_LOTH2_Condition()
 
 FUNC VOID DIA_Buddler_LOTH2_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_LOTH2_15_01"); //Od dawna tu jesteœ? 
-    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_02"); //Od kilku lat. Ja i czterech takich byliœmy ostatni¹ parti¹ ¿ywego towaru, któr¹ szajka Sandora dostarczy³a do Kolonii przed rozbiciem przez królewskich.
-    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_03"); //To siê dopiero nazywa pech.
-    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_04"); //Pech, pech i ca³e ¿ycie pech. Pechowe ma³¿eñstwo, pechowa praca – straci³em w stoczni dwa palce – i na koñcu jeszcze porwany przez bandytów i zmuszony do do¿ywotniej pracy ponad si³y w kopalni.
-    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_05"); //Chyba jedyn¹ dobr¹ rzecz¹ jaka mi siê przydarzy³a w ¿yciu jest to, ¿e Magiczna Bariera uwolni³a mnie od tej starej jêdzy.
+    AI_Output (other, self ,"DIA_Buddler_LOTH2_15_01"); //Od dawna tu jesteÅ“? 
+    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_02"); //Od kilku lat. Ja i czterech takich byliÅ“my ostatniÂ¹ partiÂ¹ Â¿ywego towaru, ktÃ³rÂ¹ szajka Sandora dostarczyÂ³a do Kolonii przed rozbiciem przez krÃ³lewskich.
+    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_03"); //To siÃª dopiero nazywa pech.
+    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_04"); //Pech, pech i caÂ³e Â¿ycie pech. Pechowe maÂ³Â¿eÃ±stwo, pechowa praca â€“ straciÂ³em w stoczni dwa palce â€“ i na koÃ±cu jeszcze porwany przez bandytÃ³w i zmuszony do doÂ¿ywotniej pracy ponad siÂ³y w kopalni.
+    AI_Output (self, other ,"DIA_Buddler_LOTH2_03_05"); //Chyba jedynÂ¹ dobrÂ¹ rzeczÂ¹ jaka mi siÃª przydarzyÂ³a w Â¿yciu jest to, Â¿e Magiczna Bariera uwolniÂ³a mnie od tej starej jÃªdzy.
 };
 
 //========================================
@@ -91,8 +91,8 @@ INSTANCE DIA_Buddler_LOTH3 (C_INFO)
    nr           = 3;
    condition    = DIA_Buddler_LOTH3_Condition;
    information  = DIA_Buddler_LOTH3_Info;
-   permanent	= FALSE;
-   description	= "Co s³ychaæ?";
+   permanent	= TRUE;
+   description	= "Co sÂ³ychaÃ¦?";
 };
 
 FUNC INT DIA_Buddler_LOTH3_Condition()
@@ -106,9 +106,9 @@ FUNC INT DIA_Buddler_LOTH3_Condition()
 
 FUNC VOID DIA_Buddler_LOTH3_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_LOTH3_15_01"); //Co s³ychaæ?
-    AI_Output (self, other ,"DIA_Buddler_LOTH3_03_02"); //Harowa³em jak dziki wó³, by móc teraz przez jakiœ czas posiedzieæ w Starym Obozie.
-    AI_Output (self, other ,"DIA_Buddler_LOTH3_03_03"); //Jaka szkoda, ¿e ten wolny czas prêdzej, czy póŸniej dobiegnie koñca i znów ca³ymi dniami bêdê musia³ tyraæ w tej przeklêtej kopalni.
+    AI_Output (other, self ,"DIA_Buddler_LOTH3_15_01"); //Co sÂ³ychaÃ¦?
+    AI_Output (self, other ,"DIA_Buddler_LOTH3_03_02"); //HarowaÂ³em jak dziki wÃ³Â³, by mÃ³c teraz przez jakiÅ“ czas posiedzieÃ¦ w Starym Obozie.
+    AI_Output (self, other ,"DIA_Buddler_LOTH3_03_03"); //Jaka szkoda, Â¿e ten wolny czas prÃªdzej, czy pÃ³Å¸niej dobiegnie koÃ±ca i znÃ³w caÂ³ymi dniami bÃªdÃª musiaÂ³ tyraÃ¦ w tej przeklÃªtej kopalni.
 };
 
 //========================================
@@ -122,7 +122,7 @@ INSTANCE DIA_Buddler_LOTH4 (C_INFO)
    condition    = DIA_Buddler_LOTH4_Condition;
    information  = DIA_Buddler_LOTH4_Info;
    permanent	= FALSE;
-   description	= "Kto tu rz¹dzi?";
+   description	= "Kto tu rzÂ¹dzi?";
 };
 
 FUNC INT DIA_Buddler_LOTH4_Condition()
@@ -136,9 +136,9 @@ FUNC INT DIA_Buddler_LOTH4_Condition()
 
 FUNC VOID DIA_Buddler_LOTH4_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_LOTH4_15_01"); //Kto tu rz¹dzi?
-    AI_Output (self, other ,"DIA_Buddler_LOTH4_03_02"); //Powiedzia³bym, ¿e bandyci, ale to miano chyba równie dobrze pasuje do wielkich kupców z gildii Araxos, posiadaczy stoczni, królewskich urzêdników i do samego króla Rhobara.
-    AI_Output (self, other ,"DIA_Buddler_LOTH4_03_03"); //W sumie to sam móg³byœ mi odpowiedzieæ na to pytanie, ale skoro je zada³eœ to raczej mi nie odpowiesz.
+    AI_Output (other, self ,"DIA_Buddler_LOTH4_15_01"); //Kto tu rzÂ¹dzi?
+    AI_Output (self, other ,"DIA_Buddler_LOTH4_03_02"); //PowiedziaÂ³bym, Â¿e bandyci, ale to miano chyba rÃ³wnie dobrze pasuje do wielkich kupcÃ³w z gildii Araxos, posiadaczy stoczni, krÃ³lewskich urzÃªdnikÃ³w i do samego krÃ³la Rhobara.
+    AI_Output (self, other ,"DIA_Buddler_LOTH4_03_03"); //W sumie to sam mÃ³gÂ³byÅ“ mi odpowiedzieÃ¦ na to pytanie, ale skoro je zadaÂ³eÅ“ to raczej mi nie odpowiesz.
 };
 
 //========================================
@@ -151,8 +151,8 @@ INSTANCE DIA_Buddler_LOTH5 (C_INFO)
    nr           = 5;
    condition    = DIA_Buddler_LOTH5_Condition;
    information  = DIA_Buddler_LOTH5_Info;
-   permanent	= FALSE;
-   description	= "Co mo¿esz mi powiedzieæ o tym miejscu?";
+   permanent	= TRUE;
+   description	= "Co moÂ¿esz mi powiedzieÃ¦ o tym miejscu?";
 };
 
 FUNC INT DIA_Buddler_LOTH5_Condition()
@@ -166,9 +166,9 @@ FUNC INT DIA_Buddler_LOTH5_Condition()
 
 FUNC VOID DIA_Buddler_LOTH5_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_LOTH5_15_01"); //Co mo¿esz mi powiedzieæ o tym miejscu?
-    AI_Output (self, other ,"DIA_Buddler_LOTH5_03_02"); //Lekko tu nie jest, ale o niebo lepiej ni¿ przy wydobyciu rudy w Starej Kopalni. Tam to dopiero ¿ycie daje w koœæ.
-    AI_Output (self, other ,"DIA_Buddler_LOTH5_03_03"); //Tutaj musisz uwa¿aæ jedynie na Bloodwyna i innych co bardziej agresywnych typków, a tam na agresywnych typków i na wszechobecne pe³zacze.
+    AI_Output (other, self ,"DIA_Buddler_LOTH5_15_01"); //Co moÂ¿esz mi powiedzieÃ¦ o tym miejscu?
+    AI_Output (self, other ,"DIA_Buddler_LOTH5_03_02"); //Lekko tu nie jest, ale o niebo lepiej niÂ¿ przy wydobyciu rudy w Starej Kopalni. Tam to dopiero Â¿ycie daje w koÅ“Ã¦.
+    AI_Output (self, other ,"DIA_Buddler_LOTH5_03_03"); //Tutaj musisz uwaÂ¿aÃ¦ jedynie na Bloodwyna i innych co bardziej agresywnych typkÃ³w, a tam na agresywnych typkÃ³w i na wszechobecne peÂ³zacze.
     AI_Output (self, other ,"DIA_Buddler_LOTH5_03_04"); //Wybierz sobie, co ci lepiej pasuje.
 };
 
@@ -184,7 +184,7 @@ INSTANCE DIA_Buddler_NIEPOKORNY1 (C_INFO)
    condition    = DIA_Buddler_NIEPOKORNY1_Condition;
    information  = DIA_Buddler_NIEPOKORNY1_Info;
    permanent	= FALSE;
-   description	= "Podobno Oned to twój kumpel.";
+   description	= "Podobno Oned to twÃ³j kumpel.";
 };
 
 FUNC INT DIA_Buddler_NIEPOKORNY1_Condition()
@@ -198,33 +198,33 @@ FUNC INT DIA_Buddler_NIEPOKORNY1_Condition()
 
 FUNC VOID DIA_Buddler_NIEPOKORNY1_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_15_01"); //Podobno Oned to twój kumpel.
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_15_01"); //Podobno Oned to twÃ³j kumpel.
     AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_03_02"); //Tak, i co ci do tego?
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_15_03"); //Nie p³aci Stra¿nikom za ochronê.
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_15_03"); //Nie pÂ³aci StraÂ¿nikom za ochronÃª.
     AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_03_04"); //To nie moja sprawa.
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_15_05"); //Teraz ju¿ twoja.
-    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_03_06"); //Bo co? Nie bêdê spiskowa³ przeciwko Onedowi!
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_15_05"); //Teraz juÂ¿ twoja.
+    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_03_06"); //Bo co? Nie bÃªdÃª spiskowaÂ³ przeciwko Onedowi!
 
     Info_ClearChoices		(DIA_Buddler_NIEPOKORNY1);
-    Info_AddChoice		(DIA_Buddler_NIEPOKORNY1, "Có¿, skoro nie rozumiesz moich s³ów to mo¿e zrozumiesz czyny!", DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE);
-    Info_AddChoice		(DIA_Buddler_NIEPOKORNY1, "Có¿, ka¿dy ma swoj¹ cenê... Zw³aszcza w tej Kolonii. ", DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY);
-    Info_AddChoice		(DIA_Buddler_NIEPOKORNY1, "Nied³ugo Oned dostanie baty od Stra¿ników , zapewne nie zapomn¹ równie¿ o jego dobrym kumplu. ", DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE);
+    Info_AddChoice		(DIA_Buddler_NIEPOKORNY1, "CÃ³Â¿, skoro nie rozumiesz moich sÂ³Ã³w to moÂ¿e zrozumiesz czyny!", DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE);
+    Info_AddChoice		(DIA_Buddler_NIEPOKORNY1, "CÃ³Â¿, kaÂ¿dy ma swojÂ¹ cenÃª... ZwÂ³aszcza w tej Kolonii. ", DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY);
+    Info_AddChoice		(DIA_Buddler_NIEPOKORNY1, "NiedÂ³ugo Oned dostanie baty od StraÂ¿nikÃ³w , zapewne nie zapomnÂ¹ rÃ³wnieÂ¿ o jego dobrym kumplu. ", DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE);
 };
 
 FUNC VOID DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE()
 {
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE_15_01"); //Có¿, skoro nie rozumiesz moich s³ów to mo¿e zrozumiesz czyny!
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE_15_01"); //CÃ³Â¿, skoro nie rozumiesz moich sÂ³Ã³w to moÂ¿e zrozumiesz czyny!
 	AI_DrawWeapon (other);
     if ((hero.attribute[ATR_STRENGTH] >= 30) && (hero.attribute[ATR_DEXTERITY] >= 20))
     {
         NiepokornyKopacz = 1;
-        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE_03_02"); //No dobrze ju¿! Schowaj ten miecz. W³aœciwie czego ode mnie chcesz?
+        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE_03_02"); //No dobrze juÂ¿! Schowaj ten miecz. WÂ³aÅ“ciwie czego ode mnie chcesz?
 		AI_RemoveWeapon (other);
     }
     else
     {
-        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE_03_03"); //Nie boje siê. Nie tak ³atwo mnie zastraszyæ!
-        B_LogEntry                     (CH1_Niepokorny_Kopacz,"Próbowa³em porozmawiaæ z Lothem. Nie da³ siê zastraszyæ.");
+        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE_03_03"); //Nie boje siÃª. Nie tak Â³atwo mnie zastraszyÃ¦!
+        B_LogEntry                     (CH1_Niepokorny_Kopacz,"PrÃ³bowaÂ³em porozmawiaÃ¦ z Lothem. Nie daÂ³ siÃª zastraszyÃ¦.");
         Log_SetTopicStatus       (CH1_Niepokorny_Kopacz, LOG_FAILED);
         MIS_Niepokorny_Kopacz = LOG_FAILED;
     };
@@ -233,19 +233,19 @@ FUNC VOID DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1SCARE()
 
 FUNC VOID DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY()
 {
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_15_01"); //Có¿, ka¿dy ma swoj¹ cenê... Zw³aszcza w Kolonii. 
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_15_01"); //CÃ³Â¿, kaÂ¿dy ma swojÂ¹ cenÃª... ZwÂ³aszcza w Kolonii. 
 	
     if Npc_HasItems(other, ItMiNugget) >= 30
     {
-        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_03_02"); //Nie najlepiej u mnie z rud¹, ledwo starcza na jedzenie… Daj 30 bry³ek i mo¿emy kontynuowaæ rozmowê. 
-        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_03_03"); //Proszê bardzo.
+        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_03_02"); //Nie najlepiej u mnie z rudÂ¹, ledwo starcza na jedzenieâ€¦ Daj 30 bryÂ³ek i moÂ¿emy kontynuowaÃ¦ rozmowÃª. 
+        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_03_03"); //ProszÃª bardzo.
         B_GiveInvItems (other, self, ItMiNugget, 30);
         NiepokornyKopacz = 1;
     }
     else
     {
-        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_03_04"); //Nie, bez tej rudy dam sobie radê. A jeœli ci nie pomogê to Onedowi pewnie te¿ nic nie bêdzie.
-        B_LogEntry                     (CH1_Niepokorny_Kopacz,"Próbowa³em porozmawiaæ z Lothem. Nie uda³o mi siê go przekupiæ.");
+        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY_03_04"); //Nie, bez tej rudy dam sobie radÃª. A jeÅ“li ci nie pomogÃª to Onedowi pewnie teÂ¿ nic nie bÃªdzie.
+        B_LogEntry                     (CH1_Niepokorny_Kopacz,"PrÃ³bowaÂ³em porozmawiaÃ¦ z Lothem. Nie udaÂ³o mi siÃª go przekupiÃ¦.");
         Log_SetTopicStatus       (CH1_Niepokorny_Kopacz, LOG_FAILED);
         MIS_Niepokorny_Kopacz = LOG_FAILED;
     };
@@ -254,15 +254,15 @@ FUNC VOID DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1BUY()
 
 FUNC VOID DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE()
 {
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE_15_01"); //Nied³ugo Oned dostanie baty od Stra¿ników, zapewne nie zapomn¹ równie¿ o jego dobrym kumplu. 
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE_15_01"); //NiedÂ³ugo Oned dostanie baty od StraÂ¿nikÃ³w, zapewne nie zapomnÂ¹ rÃ³wnieÂ¿ o jego dobrym kumplu. 
    // if (hero.attribute[ATR_DEXTERITY] >= 2)
   //  {
-        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE_03_02"); //Có¿... Jednak jest w tym trochê racji, czego chcesz?
+        AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE_03_02"); //CÃ³Â¿... Jednak jest w tym trochÃª racji, czego chcesz?
         NiepokornyKopacz = 1;
   //  }
   //  else
    // {
-        //AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE_03_03"); //To tylko gdybanie. Mo¿e ¿aden Stra¿nik go nie tknie, w koñcu wtedy inni Kopacze by go poparli.
+        //AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY1_NIEPOKORNY1PERSUADE_03_03"); //To tylko gdybanie. MoÂ¿e Â¿aden StraÂ¿nik go nie tknie, w koÃ±cu wtedy inni Kopacze by go poparli.
      //   B_LogEntry                     (CH1_Niepokorny_Kopacz,"");
     //    Log_SetTopicStatus       (CH1_Niepokorny_Kopacz, LOG_FAILED);
     //    MIS_Niepokorny_Kopacz = LOG_FAILED;
@@ -281,7 +281,7 @@ INSTANCE DIA_Buddler_NIEPOKORNY2 (C_INFO)
    condition    = DIA_Buddler_NIEPOKORNY2_Condition;
    information  = DIA_Buddler_NIEPOKORNY2_Info;
    permanent	= FALSE;
-   description	= "Skoro ju¿ ciê przekona³em to zrobisz co ci ka¿ê. Nak³onisz Oneda do wspólnej ucieczki.";
+   description	= "Skoro juÂ¿ ciÃª przekonaÂ³em to zrobisz co ci kaÂ¿Ãª. NakÂ³onisz Oneda do wspÃ³lnej ucieczki.";
 };
 
 FUNC INT DIA_Buddler_NIEPOKORNY2_Condition()
@@ -296,17 +296,17 @@ FUNC INT DIA_Buddler_NIEPOKORNY2_Condition()
 
 FUNC VOID DIA_Buddler_NIEPOKORNY2_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_01"); //Skoro ju¿ ciê przekona³em to zrobisz co ci ka¿ê. Nak³onisz Oneda do wspólnej ucieczki.
-    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_02"); //Chcesz, ¿ebyœmy siê wynieœli z tego Obozu?
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_03"); //Oszala³eœ. Gdybyœcie nawali wkrótce reszta Kopaczy by tego spróbowa³a.
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_04"); //W czasie waszej ucieczki wpadniecie w zasadzkê. 
-    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_05"); //Mamy wpaœæ w ³apy Stra¿y?
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_06"); //Nie, powinni byæ to Kopacze. To pokaza³oby ich sprzeciw wobec dzia³añ Oneda. 
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_07"); //Którzy Kopacze by siê do tego nadali?
-    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_08"); //Chocia¿by Tippler , Uscan no i pewnie Herek.
-    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_09"); //Dobrze. Pogadaj teraz z Onedem, ustal termin ucieczki na najbli¿sz¹ pó³noc.
-    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_10"); //Zrozumia³em.
-    B_LogEntry                     (CH1_Niepokorny_Kopacz,"Uda³o mi siê przekonaæ Lotha do wspó³pracy. Zaproponuje Onedowi ucieczkê z Obozu podczas której obaj wpadn¹ w przygotowan¹ przez przekupionych Kopaczy zasadzkê.");
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_01"); //Skoro juÂ¿ ciÃª przekonaÂ³em to zrobisz co ci kaÂ¿Ãª. NakÂ³onisz Oneda do wspÃ³lnej ucieczki.
+    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_02"); //Chcesz, Â¿ebyÅ“my siÃª wynieÅ“li z tego Obozu?
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_03"); //OszalaÂ³eÅ“. GdybyÅ“cie nawali wkrÃ³tce reszta Kopaczy by tego sprÃ³bowaÂ³a.
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_04"); //W czasie waszej ucieczki wpadniecie w zasadzkÃª. 
+    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_05"); //Mamy wpaÅ“Ã¦ w Â³apy StraÂ¿y?
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_06"); //Nie, powinni byÃ¦ to Kopacze. To pokazaÂ³oby ich sprzeciw wobec dziaÂ³aÃ± Oneda. 
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_07"); //KtÃ³rzy Kopacze by siÃª do tego nadali?
+    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_08"); //ChociaÂ¿by Tippler , Uscan no i pewnie Herek.
+    AI_Output (other, self ,"DIA_Buddler_NIEPOKORNY2_15_09"); //Dobrze. Pogadaj teraz z Onedem, ustal termin ucieczki na najbliÂ¿szÂ¹ pÃ³Â³noc.
+    AI_Output (self, other ,"DIA_Buddler_NIEPOKORNY2_03_10"); //ZrozumiaÂ³em.
+    B_LogEntry                     (CH1_Niepokorny_Kopacz,"UdaÂ³o mi siÃª przekonaÃ¦ Lotha do wspÃ³Â³pracy. Zaproponuje Onedowi ucieczkÃª z Obozu podczas ktÃ³rej obaj wpadnÂ¹ w przygotowanÂ¹ przez przekupionych Kopaczy zasadzkÃª.");
 };
 
 
@@ -321,7 +321,7 @@ INSTANCE DIA_Buddler_BuddlersPaid (C_INFO)
    condition    = DIA_Buddler_BuddlersPaid_Condition;
    information  = DIA_Buddler_BuddlersPaid_Info;
    permanent	= FALSE;
-   description	= "Ustali³eœ termin? ";
+   description	= "UstaliÂ³eÅ“ termin? ";
 };
 
 FUNC INT DIA_Buddler_BuddlersPaid_Condition()
@@ -338,11 +338,11 @@ FUNC INT DIA_Buddler_BuddlersPaid_Condition()
 
 FUNC VOID DIA_Buddler_BuddlersPaid_Info()
 {
-    AI_Output (other, self ,"DIA_Buddler_BuddlersPaid_15_01"); //Ustali³eœ termin?
-    AI_Output (self, other ,"DIA_Buddler_BuddlersPaid_03_02"); //Tak, o pó³nocy wiejemy.
+    AI_Output (other, self ,"DIA_Buddler_BuddlersPaid_15_01"); //UstaliÂ³eÅ“ termin?
+    AI_Output (self, other ,"DIA_Buddler_BuddlersPaid_03_02"); //Tak, o pÃ³Â³nocy wiejemy.
     AI_Output (other, self ,"DIA_Buddler_BuddlersPaid_15_03"); //Doskonale. 
     
-    B_LogEntry                     (CH1_Niepokorny_Kopacz,"Loth ustali³ termin ucieczki z Onedem. Wkrótce obaj dostan¹ nauczkê.");
+    B_LogEntry                     (CH1_Niepokorny_Kopacz,"Loth ustaliÂ³ termin ucieczki z Onedem. WkrÃ³tce obaj dostanÂ¹ nauczkÃª.");
 	
 	 NiepokornyKopacz = Wld_GetDay();
 };
